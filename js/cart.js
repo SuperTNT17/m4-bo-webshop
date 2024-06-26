60630f3b065492f6dfc2e2a4bdc27e9436bcbb3f
Robin Lemmers <r.lemmers@outlook.com>
2024-06-26 14:34:00 +0200

    functies toegevoegd en dingen verbeterd

--- js/cart.js
+++ js/cart.js
@@ -1,54 +1,5 @@
-const addToCartButtons = document.getElementsByClassName("games__button");
-let cartMessage = 0;
-const cartMessageValue = document.getElementsByClassName("shoppingCart__message")[0];
-const shoppingModal = document.getElementById("js--shoppingModal");
-let modalIsOpen = false;
-let cartItemsValues = [0, 0, 0];
-
-//
-// PROBEREN OM COOKIES TOE TE VOEGEN VOOR DIE CART
-//
-
-for (let i = 0; i < addToCartButtons.length; i++) {
-    addToCartButtons[i].onclick = function () {
-        switch (addToCartButtons[i].dataset.product) {
-            case "barbecue":
-                cartItemsValues[0] += 1;
-                break;
-            case "shirt":
-                cartItemsValues[1] += 1;
-                break;
-            case "airfryer":
-                cartItemsValues[2] += 1;
-                break;
-        }
-    }
-}
-
-let cartLink = document.getElementById("cartLink");
-const checkoutWindow = document.getElementById("js--checkoutWindow");
-let checkoutIsOpen = false;
-
-let cartItems = document.getElementsByClassName("checkout__listItem");
-let cartItems_value = document.getElementsByClassName("checkout__amount");
-
-cartLink.onclick = function () {
-    if (checkoutIsOpen === false) {
-        document.querySelector("main").style.display = "none";
-        checkoutWindow.style.display = "block";
-        checkoutIsOpen = true;
-        for (let i = 0; i < cartItems.length; i++) {
-            if (cartItemsValues[i] < 1) {
-                cartItems[i].style.display = "none";
-            }
-            else {
-                cartItems_value[i].innerHTML = cartItemsValues[i] + "x";
-                cartItems[i].style.display = "flex";
-            }
-        }
-        return;
-    }
-    document.querySelector("main").style.display = "block";
-    checkoutWindow.style.display = "none";
-    checkoutIsOpen = false;
+let fail = document.getElementById("checkout--fail");
+let button = document.getElementById("checkout--betalen");
+button.onclick = function(){
+    fail.style.display = "inline";
 }
--- js/darkmode.js
+++ js/darkmode.js
@@ -6,7 +6,7 @@ toggleButton.addEventListener('click', () => {
         toggleButton.children[1].style.animationName = "fadein-sun";
         dark = true;
     }
-    else{
+    else {
         toggleButton.children[0].style.animationName = "fadein";
         toggleButton.children[1].style.animationName = "fadeout-sun";
         dark = false;
@@ -14,5 +14,14 @@ toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
     document.querySelector('nav').classList.toggle('dark-mode');
     document.querySelector('footer').classList.toggle('dark-mode');
-    document.querySelector('#eyecatcher--content--text').classList.toggle('dark-mode');
+    
+    let path = window.location.pathname;
+    let page = path.split("/").pop();
+    if (page == "index.html") {
+        document.querySelector('#eyecatcher--content--text').classList.toggle('dark-mode');
+    }
+    if (page == "cart.html") {
+        document.querySelector('#checkout--totaal').classList.toggle('dark-mode');
+        document.querySelector('#checkout--betalen').classList.toggle('dark-mode');
+    }
 });
