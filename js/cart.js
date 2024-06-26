64cd6863823cc314216b8d14ebee54d6208eada0
Robin Lemmers <38210@ma-web.nl>
2024-06-26 16:50:18 +0200

    modal in de cart

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,10 @@
 let fail = document.getElementById("checkout--fail");
+let popup = document.getElementById("modal");
 let button = document.getElementById("checkout--betalen");
+let close = document.getElementById("close");
 button.onclick = function(){
-    fail.style.display = "inline";
+    popup.style.display = "flex";
+}
+close.onclick = function () {
+    popup.style.display = "none";
 }
